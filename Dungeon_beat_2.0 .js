f28e3428ff23671de4ef410b6758e8b4aeda8f9d
Alexander Yhde <Alexander.yhde@skola.taby.se>
2024-04-09 14:44:48 +0200

    Version 2

--- Dungeon_beat_2.0 .js
+++ Dungeon_beat_2.0 .js
@@ -5,10 +5,60 @@ canvas.height = window.innerHeight
 
 namn.innerHTML ="Dungeon Beat 2.0"
 const ctx = canvas.getContext("2d")
+const spelkaraktären_bild = document.getElementById("Spelkaraktär")
+let Facing = "upward"
 
-const spelkaraktären = document.getElementById("Spelkaraktär")
-var swidth = spelkaraktären.width;
-var sheight = spelkaraktären.height;
-let Player_picture_position_x = 320
-let Player_picture_position__y = 200
-ctx.drawImage(spelkaraktären, 120, 100, 650, 650, Player_picture_position_x, Player_picture_position__y, 100, 100);
+function Karaktär(type,sy,sx,swidth,sheight,x,y,width,height) {
+    this.type = type
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.sy = sy;
+    this.sx = sx;
+    this.swidth = swidth;
+    this.sheight = sheight;
+    this.update = function (){
+        if (Facing === "upward"){
+          ctx.drawImage(this.type,this.sy,this.sx,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
+          console.log("Updatterar")
+        }
+    }
+}
+
+document.onkeydown = function(e){
+    switch (e.key){
+        case "w":
+            ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)   
+            Spelkaraktär.y -= 10
+            let Facing = "upward"
+            console.log(Spelkaraktär.y)
+            Spelkaraktär.update()
+    }
+    switch (e.key){
+        case "s":
+            ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
+            Spelkaraktär.y += 10
+            let Facing = "downward"
+            console.log(Spelkaraktär.y)
+            Spelkaraktär.update()
+    }
+    switch (e.key){
+        case "d":
+            ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
+            Spelkaraktär.x += 10
+            console.log(Spelkaraktär.x)
+            Spelkaraktär.update()
+    }
+    switch (e.key){
+        case "a":
+            ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
+            Spelkaraktär.x -= 10
+            console.log(Spelkaraktär.x)
+            Spelkaraktär.update()
+    }
+}
+
+
+var Spelkaraktär = new Karaktär(spelkaraktären_bild, 120, 100, 650, 650, 300, 200, 100, 100);
+Spelkaraktär.update()
