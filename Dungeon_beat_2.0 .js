d56587af2197fd12ef37bb781c28f6d69e4161c9
Alexander Yhde <Alexander.yhde@skola.taby.se>
2024-04-12 17:41:58 +0200

    Version 5

--- Dungeon_beat_2.0 .js
+++ Dungeon_beat_2.0 .js
@@ -2,12 +2,12 @@ const Game_canvas = document.getElementById("Game_canvas")
 let namn = document.getElementById("Spelnamn")
 Game_canvas.width = window.innerWidth
 Game_canvas.height = window.innerHeight
-
 namn.innerHTML ="Dungeon Beat 2.0"
 const ctx = Game_canvas.getContext("2d")
-const spelkaraktären_bild = document.getElementById("Spelkaraktär")
+const Warrior_spelkaraktären_bild = document.getElementById("Spelkaraktär")
 var Current_room = "Main_Dungeon"
 var Facing = "Downward"
+var animation_active = false
 
 function Karaktär(type,swidth,sheight,x,y,width,height,sx) {
     this.type = type
@@ -21,7 +21,7 @@ function Karaktär(type,swidth,sheight,x,y,width,height,sx) {
     this.update = function (){
         if (Facing === "Downward"){
           this.sy = 0;
-          ctx.drawImage(this.type,this.sx,this.sy,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
+          ctx.drawImage(type,this.sx,this.sy,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
         }
         if (Facing === "Upward"){
             this.sy = 700
@@ -61,8 +61,9 @@ document.onkeydown = function(e){
             ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
             Facing = "Upward"
             Spelkaraktär.update()
+            Player_animation("Walking")
             if (Spelkaraktär.y >= Update_and_Assign_Room().Top_border){
-                Spelkaraktär.y -= 10
+                Spelkaraktär.y -= 6
             }
     }
     switch (e.key){
@@ -70,9 +71,9 @@ document.onkeydown = function(e){
             ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
             Facing = "Downward"
             Spelkaraktär.update()
+            Player_animation("Walking")
             if (Spelkaraktär.y <= Update_and_Assign_Room().Bottom_border ){
-                Spelkaraktär.y += 10
-                Player_animation("Walking")
+                Spelkaraktär.y += 6
             }
     }
     switch (e.key){
@@ -80,8 +81,9 @@ document.onkeydown = function(e){
             ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
             Facing = "Right"
             Spelkaraktär.update()
+            Player_animation("Walking")
             if (Spelkaraktär.x <= Update_and_Assign_Room().right_border ){
-                Spelkaraktär.x += 10
+                Spelkaraktär.x += 6
             }
     }
     switch (e.key){
@@ -89,44 +91,56 @@ document.onkeydown = function(e){
             ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
             Facing = "Left"
             Spelkaraktär.update()
+            Player_animation("Walking")
             if (Spelkaraktär.x >= Update_and_Assign_Room().Left_border ){
-                Spelkaraktär.x -= 10
+                Spelkaraktär.x -= 6
             }
+    }
+    switch (e.key) {
+        case "z":
+            Player_animation("Attack")
     }
 }
 
 function Player_animation(Character_action){
-    var latest_Timestamp = 0
-    timestamp = 0
-    if (Character_action === "Walking" && latest_Timestamp === 0){
-        var Current_frame = 1
+    if (animation_active) return;
+
+    if (Character_action === "Walking"){
+        var Current_frame = 0
         var Frame_to_end_with = 3
-    
-        function animate(timestamp){
-            if (timestamp - latest_Timestamp < 1000){
-              requestAnimationFrame(animate)
-            }
+        var Time_To_animate = 250
+    }
+    if (Character_action === "Attack"){
+        var Current_frame = 0
+        var Frame_to_end_with = 5
+        Spelkaraktär.sx = 2295
+        var Time_To_animate = 89
+    }
+
+    animation_active = true
+    var Interval = setInterval(animate,Time_To_animate)
+    function animate(){
+            Current_frame++
             ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
-            latest_Timestamp = timestamp
-            Spelkaraktär.sx = 800 * Current_frame
+            Spelkaraktär.sx += 773
             Spelkaraktär.update()
-            Current_frame += 1
-        }
-        }
-    if (Current_frame <= Frame_to_end_with){
-            requestAnimationFrame(animate)
-    }
-    if (Current_frame = Frame_to_end_with){
-            console.log("return")
-            Spelkaraktär.sx = 0
-            Spelkaraktär.update()
-            return
-    }
+            Update_and_Assign_Room()
+            if (Current_frame === Frame_to_end_with){
+                clearInterval(Interval)
+                ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
+                Spelkaraktär.sx = 0
+                Spelkaraktär.update()
+                animation_active = false
+                Update_and_Assign_Room()
+                return
+            }
+     }
 }
 
-var Spelkaraktär = new Karaktär(spelkaraktären_bild, 760, 760, 300, 200, 220, 190,0);
+
+var Spelkaraktär = new Karaktär(Warrior_spelkaraktären_bild, 760, 760, 300, 200, 220, 190,0);
     Spelkaraktär.update()
-    var Main_dungeon = new Room("Bilder/Main_Dungeon_background.png",document.getElementById("Main_Dungeon_walls"),140,0,0,1090)
+    var Main_dungeon = new Room("Bilder/Main_Dungeon_background.png",document.getElementById("Main_Dungeon_walls"),140,430,0,1090)
     Main_dungeon.update()
 
 
