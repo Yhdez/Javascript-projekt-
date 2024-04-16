4c79fcad65f49b2d66e00f89ee173c83a3d281bc
Alexander Yhde <Alexander.yhde@skola.taby.se>
2024-04-16 14:45:22 +0200

    Version 7

--- Dungeon_beat_2.0 .js
+++ Dungeon_beat_2.0 .js
@@ -7,7 +7,16 @@ const ctx = Game_canvas.getContext("2d")
 const Warrior_spelkaraktären_bild = document.getElementById("Warrior")
 const Health_bild = document.getElementById("Health")
 var Facing = "Downward"
+
 var Player_animation_active = false
+var Monster_Current_frame = undefined
+var Monster_Frame_to_end_with = undefined
+var Monster_interval = undefined
+var Monster_walking_interval = undefined
+var Monster_animation_times_repeated = undefined
+var Asssigned_x_position = undefined
+var Assigned_y_position = undefined
+var Monster_walking_active = undefined
 
 function Karaktär(type,swidth,sheight,x,y,width,height,sx,sy) {
     this.type = type
@@ -40,7 +49,7 @@ function Karaktär(type,swidth,sheight,x,y,width,height,sx,sy) {
     this.Monster_update = function(){
         ctx.drawImage(this.type,this.sx,this.sy,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
     }
-}
+    }
 
 function Room(type,wall_type,Top_border,Bottom_border,Left_border,right_border){
    this.type = type
@@ -55,23 +64,78 @@ function Room(type,wall_type,Top_border,Bottom_border,Left_border,right_border){
    }
 }
 
-function Monster_encounter(type){
-  if (type = 1){ //Devil
-    var Monster_health = 5
-    var Devil = new Karaktär(document.getElementById("Devil"), 600, 600, 800, 200, Game_canvas.width*0.2, Game_canvas.height*0.32,0,0)
-    Devil.Monster_update()
-    console.log(document.getElementById("Devil").height)
-    requestAnimationFrame(Monster_animation.bind(null,"idle",Devil))
+function Monster_encounter(monster_type){
+
+  if (monster_type.type === document.getElementById("Devil")){ 
+      Idle()
+      setTimeout(Run_and_swing,4000)
+ }
+  function Monster_animation(){
+    Monster_Current_frame++
+    ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
+    monster_type.sx = 630 * Monster_Current_frame
+    monster_type.Monster_update()
+    if (Monster_Current_frame === Monster_Frame_to_end_with){
+        ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
+        if (Monster_animation_times_repeated === 1){
+            clearInterval(Monster_interval)
+            Idle()
+        }
+        monster_type.sx = 0
+        monster_type.Monster_update()
+        Monster_Current_frame = 0
+    }
+  }
+
+  function Idle(){
+    Monster_animation_times_repeated = "Indefinitely"
+    monster_type.sy = 0
+    Monster_Current_frame = -1
+    Monster_Frame_to_end_with = 3
+    Monster_interval = setInterval(Monster_animation,200)
+  }
+
+  function Run_and_swing(){
+    Assigned_y_position = (Math.random() * 80) - 60
+    Asssigned_x_position = (Math.random() + 50)*5
+    Monster_walking_interval = setInterval(Go_to_random_position,100)
+    if (Monster_walking_active === "No"){
+    monster_type.sy = 600
+    Monster_Current_frame = -1
+    Monster_Frame_to_end_with = 7
+    Monster_animation_times_repeated = 1
+    clearInterval(Monster_interval)
+    Monster_interval = setInterval(Monster_animation,100)
+    console.log("swing")
+    }
+  }
+
+  function Go_to_random_position(){
+    console.log(Assigned_y_position)
+    if (monster_type.x >= Asssigned_x_position){
+        clearInterval(Monster_walking_interval)
+        Monster_walking_active = "No"
+        return
+    }
+    monster_type.x += (30)
   }
 }
 
 function Update_and_Assign_Room(){
-    if (Room.type = "Main_Dungeon_walls"){Main_dungeon.update(); return Main_dungeon}
+    if (Room.type = "Main_Dungeon_walls"){Main_dungeon.update();return Main_dungeon}
+}
+
+function assign_monster(type){
+   if (type === 1){
+    var Devil = new Karaktär(document.getElementById("Devil"), 600, 600, 800, 200, Game_canvas.width*0.2, Game_canvas.height*0.32,0,0)
+    return Devil
+   }
 }
 
 function Check_health(){
-    if (health === 5){ ctx.drawImage(Health_bild,sx = 0, sy = 0, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 400, width = 200)}
-    if (health === 4){ ctx.drawImage(Health_bild,sx = 0, sy = 850, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
+    if (health === 5){ ctx.drawImage(Health_bild,sx = 0, sy = 0, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
+    if (health === 4){ ctx.drawImage(Health_bild,sx = 0, sy = 880, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
+    if (health === 3){ ctx.drawImage(Health_bild,sx = 0, sy = 1760, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
 }
 
 document.onkeydown = function(e){
@@ -155,42 +219,13 @@ function Player_animation(Character_action){
 
 }
 
-function Monster_animation(monster_action,monster_type){
-    if (monster_action === "idle"){
-        var Current_frame = 0
-        var Frame_to_end_with = 3
-        var Time_To_animate = 200
-    }
-    if (monster_action === "Attack"){
-        var Current_frame = 0
-        var Frame_to_end_with = 5
-        monster_type.sx = 2295
-        var Time_To_animate = 89
-    }
-    function animate(){
-        Current_frame++
-        ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
-        monster_type.sx += 773
-        Spelkaraktär.update()
-        console.log("körd")
-        if (Current_frame === Frame_to_end_with){
-            clearInterval(Interval)
-            ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
-            monster_type.sx = 0
-            monster_type.update()
-            return
-        }
-    }
-    var Interval = setInterval(animate,Time_To_animate)
-}
-
 var Spelkaraktär = new Karaktär(Warrior_spelkaraktären_bild, 760, 760, 300, 200, Game_canvas.width*0.2, Game_canvas.height*0.35,0,0);
     Spelkaraktär.update()
-    health = 4
+    health = 3
     Check_health()
-    var Main_dungeon = new Room("Bilder/Main_Dungeon_background.png",document.getElementById("Main_Dungeon_walls"),140,430,0,1090)
+    var Main_dungeon = new Room("Bilder/Main_Dungeon_background.png",document.getElementById("Main_Dungeon_walls"),140,430,-35,1090)
     Main_dungeon.update()
-    Monster_encounter(1)
+    Monster_encounter(assign_monster(1))
 
 
 
