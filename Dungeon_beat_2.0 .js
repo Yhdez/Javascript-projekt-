ff6476c399b85fd859bb6e64d077b068e2118576
Alexander Yhde <Alexander.yhde@skola.taby.se>
2024-04-16 20:22:47 +0200

    version 8

--- Dungeon_beat_2.0 .js
+++ Dungeon_beat_2.0 .js
@@ -16,9 +16,8 @@ var Monster_walking_interval = undefined
 var Monster_animation_times_repeated = undefined
 var Asssigned_x_position = undefined
 var Assigned_y_position = undefined
-var Monster_walking_active = undefined
 
-function Karaktär(type,swidth,sheight,x,y,width,height,sx,sy) {
+function Karaktär(type,swidth,sheight,x,y,width,height,sx,sy,health) {
     this.type = type
     this.width = width;
     this.height = height;
@@ -28,10 +27,11 @@ function Karaktär(type,swidth,sheight,x,y,width,height,sx,sy) {
     this.sheight = sheight;
     this.sx = sx
     this.sy = sy
+    this.health = health
     this.update = function (){
         if (Facing === "Downward"){
           this.sy = 0;
-          ctx.drawImage(this.type,this.sx,this.sy,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
+          ctx.drawImage(this.type,this.sx ,this.sy,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
         }
         if (Facing === "Upward"){
             this.sy = 700
@@ -49,7 +49,7 @@ function Karaktär(type,swidth,sheight,x,y,width,height,sx,sy) {
     this.Monster_update = function(){
         ctx.drawImage(this.type,this.sx,this.sy,this.swidth,this.sheight,this.x,this.y,this.width,this.height)
     }
-    }
+}
 
 function Room(type,wall_type,Top_border,Bottom_border,Left_border,right_border){
    this.type = type
@@ -65,15 +65,19 @@ function Room(type,wall_type,Top_border,Bottom_border,Left_border,right_border){
 }
 
 function Monster_encounter(monster_type){
-
+    clearInterval(Monster_interval)
   if (monster_type.type === document.getElementById("Devil")){ 
       Idle()
-      setTimeout(Run_and_swing,4000)
+      setTimeout(Run_and_swing,(Math.random() * 4000) + 2000)
+      setTimeout(Projectile,9000)
+      if (monster_type.health > 0){
+        setTimeout(Monster_encounter.bind(null,monster_type),17000)
+      }
  }
   function Monster_animation(){
     Monster_Current_frame++
     ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
-    monster_type.sx = 630 * Monster_Current_frame
+    monster_type.sx = 625 * Monster_Current_frame
     monster_type.Monster_update()
     if (Monster_Current_frame === Monster_Frame_to_end_with){
         ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
@@ -92,32 +96,81 @@ function Monster_encounter(monster_type){
     monster_type.sy = 0
     Monster_Current_frame = -1
     Monster_Frame_to_end_with = 3
-    Monster_interval = setInterval(Monster_animation,200)
+    Monster_interval = setInterval(Monster_animation,150)
   }
 
   function Run_and_swing(){
-    Assigned_y_position = (Math.random() * 80) - 60
-    Asssigned_x_position = (Math.random() + 50)*5
-    Monster_walking_interval = setInterval(Go_to_random_position,100)
-    if (Monster_walking_active === "No"){
+    Assigned_y_position = (Math.random() * 200) + 180
+    Asssigned_x_position = (Math.random() * 901) +30
+    Go_to_random_position()
     monster_type.sy = 600
     Monster_Current_frame = -1
     Monster_Frame_to_end_with = 7
     Monster_animation_times_repeated = 1
     clearInterval(Monster_interval)
-    Monster_interval = setInterval(Monster_animation,100)
-    console.log("swing")
-    }
+    Monster_interval = setInterval(Monster_animation,200)
   }
 
   function Go_to_random_position(){
-    console.log(Assigned_y_position)
-    if (monster_type.x >= Asssigned_x_position){
-        clearInterval(Monster_walking_interval)
-        Monster_walking_active = "No"
-        return
+    if (monster_type.y <= Assigned_y_position){
+        if (monster_type.x <= Asssigned_x_position){Monster_walking_interval = setInterval(if_both_bigger_than_monster,100)}
+        else{Monster_walking_interval = setInterval(if_x_smaller_than_monster ,200)}
     }
-    monster_type.x += (30)
+    else{
+        if(monster_type <= Asssigned_x_position){Monster_walking_interval = setInterval(if_y_smaller_than_monster ,100)}
+        else{Monster_walking_interval = setInterval(if_both_smaller_than_monster ,150)}
+    }
+
+    function if_y_smaller_than_monster(){
+        if (monster_type.x >= Asssigned_x_position && monster_type.y <= Assigned_y_position){
+            clearInterval(Monster_walking_interval)
+            return
+        }
+        ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
+        if(monster_type.x < Asssigned_x_position){monster_type.x += 15}
+        if (monster_type.y > Assigned_y_position){monster_type.y -= 15}
+        monster_type.Monster_update()
+    }
+    function if_x_smaller_than_monster(){
+        if (monster_type.x <= Asssigned_x_position && monster_type.y >= Assigned_y_position){
+            clearInterval(Monster_walking_interval)
+            return
+        }
+        ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
+        if(monster_type.x > Asssigned_x_position){monster_type.x -= 15}
+        if (monster_type.y < Assigned_y_position){monster_type.y += 15}
+        monster_type.Monster_update()
+    }
+    function if_both_smaller_than_monster(){
+        if (monster_type.x <= Asssigned_x_position && monster_type.y <= Assigned_y_position){
+            clearInterval(Monster_walking_interval)
+            return
+        }
+        ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
+        if(monster_type.x > Asssigned_x_position){monster_type.x -= 15}
+        if (monster_type.y > Assigned_y_position){monster_type.y -= 15}
+        monster_type.Monster_update()
+    }
+    function if_both_bigger_than_monster(){
+        if (monster_type.x >= Asssigned_x_position && monster_type.y >= Assigned_y_position){
+           clearInterval(Monster_walking_interval)
+           return
+    }
+        ctx.clearRect(monster_type.x,monster_type.y,monster_type.width,monster_type.height)
+        if(monster_type.x < Asssigned_x_position){monster_type.x += 15}
+        if (monster_type.y < Assigned_y_position){monster_type.y += 15}
+        monster_type.Monster_update()
+    }
+  }
+
+  function Projectile(shots){
+    monster_type.sy = 1320
+    monster_type.sx = 0
+    Monster_Current_frame = -1
+    Monster_Frame_to_end_with = 7
+    Monster_animation_times_repeated = 1
+    clearInterval(Monster_interval)
+    Monster_interval = setInterval(Monster_animation,120)
   }
 }
 
@@ -127,15 +180,15 @@ function Update_and_Assign_Room(){
 
 function assign_monster(type){
    if (type === 1){
-    var Devil = new Karaktär(document.getElementById("Devil"), 600, 600, 800, 200, Game_canvas.width*0.2, Game_canvas.height*0.32,0,0)
+    var Devil = new Karaktär(document.getElementById("Devil"), 600, 600, 600, 400, Game_canvas.width*0.2, Game_canvas.height*0.32,0,0,5)
     return Devil
    }
 }
 
 function Check_health(){
-    if (health === 5){ ctx.drawImage(Health_bild,sx = 0, sy = 0, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
-    if (health === 4){ ctx.drawImage(Health_bild,sx = 0, sy = 880, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
-    if (health === 3){ ctx.drawImage(Health_bild,sx = 0, sy = 1760, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
+    if (Spelkaraktär.health === 5){ ctx.drawImage(Health_bild,sx = 0, sy = 0, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
+    if (Spelkaraktär.health === 4){ ctx.drawImage(Health_bild,sx = 0, sy = 880, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
+    if (Spelkaraktär.health === 3){ ctx.drawImage(Health_bild,sx = 0, sy = 1760, swidth = Health_bild.width, sheight = Health_bild.height / 4.5, x = 950, y= -10, height = 350, width = 200)}
 }
 
 document.onkeydown = function(e){
@@ -194,14 +247,14 @@ function Player_animation(Character_action){
     }
     if (Character_action === "Attack"){
         var Current_frame = 0
-        var Frame_to_end_with = 5
-        Spelkaraktär.sx = 2295
+        var Frame_to_end_with = 6
+        Spelkaraktär.sx = 1540
         var Time_To_animate = 89
     }
     function animate(){
         Current_frame++
         ctx.clearRect(Spelkaraktär.x,Spelkaraktär.y,Spelkaraktär.width,Spelkaraktär.height)
-        Spelkaraktär.sx += 773
+        Spelkaraktär.sx += 770
         Spelkaraktär.update()
         Update_and_Assign_Room()
         if (Current_frame === Frame_to_end_with){
@@ -219,7 +272,7 @@ function Player_animation(Character_action){
 
 }
 
-var Spelkaraktär = new Karaktär(Warrior_spelkaraktären_bild, 760, 760, 300, 200, Game_canvas.width*0.2, Game_canvas.height*0.35,0,0);
+var Spelkaraktär = new Karaktär(Warrior_spelkaraktären_bild, 760, 760, 300, 200, Game_canvas.width*0.2, Game_canvas.height*0.35,0,0,5);
     Spelkaraktär.update()
     health = 3
     Check_health()
@@ -227,5 +280,6 @@ var Spelkaraktär = new Karaktär(Warrior_spelkaraktären_bild, 760, 760, 300, 2
     Main_dungeon.update()
     Monster_encounter(assign_monster(1))
 
+    ctx.drawImage(Warrior_spelkaraktären_bild, 760, 760, 300, 200, Game_canvas.width*0.2, Game_canvas.height*0.35,1550,0,5)
 
 
